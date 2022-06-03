b85a2f975eb43c112ba577fca415c7524f5fad1d
bcri25 <cyrine.boudaya@campus.tu-berlin.de>
2022-06-03 18:49:17 +0200

    new changes regarding data type and number of uploaded data

--- src/drag_drop/Drag.js
+++ src/drag_drop/Drag.js
@@ -1,12 +1,14 @@
 import React, { useState, useRef } from 'react';
 import {Form} from 'react-bootstrap';
-import Dropzone from 'react-dropzone';
+import Dropzone,{useDropzone} from 'react-dropzone';
+
 
 const Drag = ()  => {
 
   const [file, setFile] = useState(null); 
-  const [setPreviewSrc] = useState(''); 
+  const [previewSrc,setPreviewSrc] = useState(''); 
   const dropRef = useRef(); // React ref for managing the hover state of droppable area  
+  const [files$, setFiles] = useState([]);
 
   const onDrop = (files) => {
     const [uploadedFile] = files;
@@ -17,11 +19,33 @@ const Drag = ()  => {
     };
     fileReader.readAsDataURL(uploadedFile);
     
+    console.log( acceptedFiles,fileRejections,files$);
+    
   };
 
   const handleOnSubmit = async (event) => {
     event.preventDefault();
   };  
+  const {
+    acceptedFiles,
+    fileRejections,
+    getRootProps,
+    getInputProps
+  } = useDropzone({
+      accept: {
+      'text/csv': ['.csv'],
+      'application/vnd.ms-excel': ['.xls'] ,
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' : []
+    }, 
+    maxFiles:1,
+    
+    onDrop: acceptedFiles => {
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })));
+      console.log(acceptedFiles,fileRejections);
+    }
+  });
   return (
     <React.Fragment>
       <Form  onSubmit={handleOnSubmit}>
@@ -29,8 +53,7 @@ const Drag = ()  => {
     
         <div className="upload-section"> 
            <div>
-             <Dropzone onDrop={onDrop}>
-                 {({ getRootProps, getInputProps }) => (
+               
                   <div {...getRootProps({ className: 'drop-zone' })} ref={dropRef}>
                        <input {...getInputProps()} />
                        <p>please select the file...</p>
@@ -40,8 +63,6 @@ const Drag = ()  => {
                                           </div>
                                           )}
                     </div>
-                  )}
-             </Dropzone>
             </div>
        </div>
       </Form>
