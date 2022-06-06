5b5cb0fd7d172a2dad84ca47c75fa1cec11e19f8
dittmanndennis <dennis1dittmann@gmail.com>
2022-06-06 15:15:46 +0200

    cosmetics

--- src/drag_drop/table/Table.js
+++ src/drag_drop/table/Table.js
@@ -1,8 +1,5 @@
-import { read, write } from 'xlsx';
 
 function Table(props) {
-
-  const doc = new DOMParser().parseFromString(props.data, 'text/html');
 
   return (
     <div className="Table" dangerouslySetInnerHTML={{__html: props.data}}>
