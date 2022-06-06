07c864440f7940f944fdef8e0911980b67b2cacc
dittmanndennis <dennis1dittmann@gmail.com>
2022-06-06 15:14:44 +0200

    initial version of reading and writing tables

--- src/drag_drop/table/Table.js
+++ src/drag_drop/table/Table.js
@@ -2,11 +2,10 @@ import { read, write } from 'xlsx';
 
 function Table(props) {
 
-  const inner = {}
+  const doc = new DOMParser().parseFromString(props.data, 'text/html');
 
   return (
-    <div className="Table">
-      {props.data}
+    <div className="Table" dangerouslySetInnerHTML={{__html: props.data}}>
     </div>
   );
 
