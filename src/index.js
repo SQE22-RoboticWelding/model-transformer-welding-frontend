5a7b843570f05d2a5c0e5a0343a7f9e80f47bf7a
Dennis Dittmann <40322643+dittmanndennis@users.noreply.github.com>
2022-06-21 17:46:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,35 +7,21 @@ import './styles.scss';
 
 import Drag from './drag_drop/Drag';
 
-const DragRouter = () => (
-  <BrowserRouter>
-    <div className="container">
-      <div className="main-content">
-        <Switch>
-          <Route component={Drag} path="/" exact={true} />
-        </Switch>
+export default function DragRouter() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <div className="main-content">
+          <Switch>
+            <Route component={Drag} path="/" exact={true} />
+          </Switch>
+        </div>
       </div>
-    </div>
-  </BrowserRouter>
-);
+    </BrowserRouter>
+  );
+}
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(<DragRouter />);
-
-/*import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(); */
