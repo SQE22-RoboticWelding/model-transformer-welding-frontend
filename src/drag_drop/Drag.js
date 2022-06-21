31441c4da5fdfaefe4d4528a7398e3ce6e87fb0e
Dennis Dittmann <40322643+dittmanndennis@users.noreply.github.com>
2022-06-21 17:44:12 +0200

    Update Drag.js

--- src/drag_drop/Drag.js
+++ src/drag_drop/Drag.js
@@ -7,7 +7,7 @@ import GenerateTable from './table/Table';
 function Drag() {
 
   const [file, setFile] = useState(null);
-  const [previewSrc, setPreviewSrc] = useState('');
+  const [previewSrc, setPreviewSrc] = useState(null);
   const dropRef = useRef(); // React ref for managing the hover state of droppable area
 
   const onDrop = (files) => {
@@ -15,14 +15,12 @@ function Drag() {
     setFile(uploadedFile);
 
     const fileReader = new FileReader();
-    fileReader.onload = () => {
-
-      var data = new Uint8Array(fileReader.result);
-      var wb = read(data, { type:'array' });
-      var wsName = wb.SheetNames[0];
-      var jsonSheet = utils.sheet_to_json(wb.Sheets[wsName]);
+    fileReader.onload = () => {                       // expecting only one sheet. therefore, only ready sheet at index 0.
+      const data = new Uint8Array(fileReader.result);
+      const wb = read(data, { type:'array' });
+      const wsName = wb.SheetNames[0];
+      const jsonSheet = utils.sheet_to_json(wb.Sheets[wsName]);
       setPreviewSrc(jsonSheet);
-      
     };
     fileReader.readAsArrayBuffer(uploadedFile);
     dropRef.current.style.border = '2px dashed #e9ebeb';
@@ -36,35 +34,33 @@ function Drag() {
     }
   };
 
-  const handleOnSubmit = async (event) => {
+  const handleOnSubmit = (event) => {
     event.preventDefault();
   };
 
   return (
     <React.Fragment>
-      <div className="upload-section" onSubmit={handleOnSubmit}> 
-        <div>
-          <Dropzone accept='.xlsx,.xls,.csv' multiple={false}
-          onDrop={onDrop}
-                    onDragEnter={() => updateBorder('over')}
-                    onDragLeave={() => updateBorder('leave')}
-          >
-            {({ getRootProps, getInputProps }) => (
-              <div {...getRootProps({ className: 'drop-zone' })} ref={dropRef}>
-                <input {...getInputProps()} />
-                <p>Drag and drop a file OR click here to select a file.</p>
-                {file && (
-                  <div>
-                    <strong>Selected file:</strong> {file.name}
-                  </div>
-                )}
-              </div>
-            )}
-          </Dropzone>
-        </div>
+      <div className="upload-section" onSubmit={handleOnSubmit}>
+        <Dropzone accept='.xlsx,.xls,.csv' multiple={false}
+                  onDrop={onDrop}
+                  onDragEnter={() => updateBorder('over')}
+                  onDragLeave={() => updateBorder('leave')}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <div {...getRootProps({ className: 'drop-zone' })} ref={dropRef}>
+              <input {...getInputProps()} />
+              <p>Drag and drop a file OR click here to select a file.</p>
+              {file && (
+                <div>
+                  <b>Selected file:</b> {file.name}
+                </div>
+              )}
+            </div>
+          )}
+        </Dropzone>
       </div>
       {previewSrc && (
-        <GenerateTable data = {previewSrc} />
+        <GenerateTable data={previewSrc} />
       )}
     </React.Fragment>
   );
