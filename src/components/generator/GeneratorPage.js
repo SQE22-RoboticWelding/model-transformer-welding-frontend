1b551697ce9e3a8a7f71eeadabdf549ce56110fd
dittmanndennis <dennis1dittmann@gmail.com>
2022-06-27 15:35:18 +0200

    initial mui config

--- src/components/generator/GeneratorPage.js
+++ src/components/generator/GeneratorPage.js
@@ -1,75 +1,66 @@
-import styled from "styled-components";
 import {useEffect, useState} from "react";
 import FetchHandler from "../common/FetchHandler";
 import Settings from "../common/settings";
 import Notifications from "../common/Notifications";
+import { Button, Paper, styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 
-const GeneratorPageRoot = styled.div`
-  width: 100%;
-  display: flex;
+const GeneratorPageRoot = styled('div')({
+  width: '100%',
+  /*display: flex;
   flex-direction: column;
   align-items: center;
 
   > * {
     margin-bottom: 16px;
-  }
-`;
+  }*/
+});
 
-const TableRoot = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  box-shadow: 0 0 8px 0 #8E8E8E;
-  border-radius: 8px;
-`;
-const TableHeader = styled.thead`
-  width: 100%;
-`;
+const StyledTable = styled(Table)({
+  //color: 'darkslategray',
+  width: '100%',
+  //border-collapse: collapse,
+  //box-shadow: 0 0 8px 0 #8E8E8E,
+  //border-radius: 8px,
+});
+const StyledTableHead = styled(TableHead)({
+  width: '100%',
+});
 
-const TableRow = styled.tr`
-  > * {
+const StyledHeadTableRow = styled(TableRow)({
+  /*> * {
     padding: 2em;
-
     :nth-child(odd) {
       background-color: #EFEFEF;
     }
-
     :nth-child(even) {
       background-color: #DFDFDF;
     }
-  }
-`;
+  }*/
+});
 
-const BodyTableRow = styled(TableRow)`
-  > * {
+const StyledBodyTableRow = styled(TableRow)({
+  /*> * {
     border-top: 1px solid black;
-  }
-`;
+  }*/
+});
 
-const CellHeader = styled.th`
-  border-bottom: 2px solid black;
-`;
-const CellSingleHeader = styled(CellHeader)`
-  width: calc(100% * 1 / 5);
-`;
-const CellDoubleHeader = styled(CellHeader)`
-  width: calc(100% * 2 / 5);
-`;
+const StyledTableCell = styled(TableCell)({
+  //border-bottom: 2px solid black,
+});
+const StyledSingleTableCell = styled(StyledTableCell)({
+  width: '20%',
+});
+const StyledDoubleTableCell = styled(StyledTableCell)({
+  width: '40%',
+});
 
-const CellSingle = styled.td`
-  width: calc(100% * 1 / 5);
-`;
-const CellDouble = styled.td`
-  width: calc(100% * 2 / 6);
-`;
-
-const GenerateButton = styled.td`
-  cursor: pointer;
-  width: 128px;
-
+const StyledButton = styled(Button)({
+  /*cursor: pointer,
+  width: 128px,
   :hover {
-    background-color: #BFBFBF;
-  }
-`;
+    background-color: #BFBFBF,
+  },*/
+});
 
 const GeneratorPage = () => {
     const [projectRetrievalState, setProjectRetrievalState] = useState("idle");
@@ -87,31 +78,37 @@ const GeneratorPage = () => {
     return (
         <GeneratorPageRoot>
             {(projectRetrievalState === "success" && availableProjects.length > 0) ? (
-                <TableRoot>
-                    <TableHeader>
-                        <TableRow>
-                            <CellSingleHeader>Name</CellSingleHeader>
-                            <CellDoubleHeader>Description</CellDoubleHeader>
-                            <CellSingleHeader>Created</CellSingleHeader>
-                            <CellSingleHeader>Last Modification</CellSingleHeader>
-                            <CellHeader/>
-                        </TableRow>
-                    </TableHeader>
+              <TableContainer component={Paper}>
+                <StyledTable>
+                    <StyledTableHead>
+                        <StyledHeadTableRow>
+                            <StyledSingleTableCell>Name</StyledSingleTableCell>
+                            <StyledDoubleTableCell>Description</StyledDoubleTableCell>
+                            <StyledSingleTableCell>Created</StyledSingleTableCell>
+                            <StyledSingleTableCell>Last Modification</StyledSingleTableCell>
+                            <StyledSingleTableCell/>
+                            <StyledSingleTableCell/>
+                        </StyledHeadTableRow>
+                    </StyledTableHead>
 
-                    <tbody>
+                    <TableBody>
                     {availableProjects.map((project) => (
-                        <BodyTableRow key={project.id}>
-                            <CellSingle>{project.name}</CellSingle>
-                            <CellDouble>{project.description}</CellDouble>
-                            <CellSingle>{new Date(project.created_at).toLocaleString("de-DE")}</CellSingle>
-                            <CellSingle>{new Date(project.modified_at).toLocaleString("de-DE")}</CellSingle>
-                            <GenerateButton onClick={() => Notifications.notify("Not implemented yet", "warning")}>
-                                Generate | Edit
-                            </GenerateButton>
-                        </BodyTableRow>
+                        <StyledBodyTableRow key={project.id}>
+                            <StyledSingleTableCell>{project.name}</StyledSingleTableCell>
+                            <StyledDoubleTableCell>{project.description}</StyledDoubleTableCell>
+                            <StyledSingleTableCell>{new Date(project.created_at).toLocaleString("de-DE")}</StyledSingleTableCell>
+                            <StyledSingleTableCell>{new Date(project.modified_at).toLocaleString("de-DE")}</StyledSingleTableCell>
+                            <StyledButton onClick={() => Notifications.notify("Not implemented yet", "warning")}>
+                                Edit
+                            </StyledButton>
+                            <StyledButton onClick={() => Notifications.notify("Not implemented yet", "warning")}>
+                                Generate
+                            </StyledButton>
+                        </StyledBodyTableRow>
                     ))}
-                    </tbody>
-                </TableRoot>
+                    </TableBody>
+                </StyledTable>
+              </TableContainer>
             ) : projectRetrievalState === "success" ? (
                 <p>There are no projects yet</p>
             ) : projectRetrievalState === "loading" &&
