96e31397c167a64c5c0f6eb80709a47d25f56854
dolevi-tub <d.genz@campus.tu-berlin.de>
2022-06-27 20:02:57 +0200

    implement robot operation

---
+++ src/components/robot/RobotDeletor.js
@@ -0,0 +1,85 @@
+import {
+    Button,
+    Container,
+    Dialog,
+    DialogTitle,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Typography
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {useState} from "react";
+import FetchHandler from "../common/FetchHandler";
+import Settings from "../common/settings";
+import Notifications from "../common/Notifications";
+import {ListItemSpreadingChildren} from "../common/StyledComponents";
+
+
+const deleteRobot = (id) => {
+    const urlParams = new URLSearchParams({_id: id});
+    return new Promise((resolve, reject) => {
+        FetchHandler.simple(fetch(`${Settings.robotPath}/:id?${urlParams}`, {method: "DELETE"}))
+            .then(() => {
+                Notifications.notify("Removed robot.", "success");
+                resolve();
+            })
+            .catch((err) => {
+                Notifications.notify(`Failed to remove robot.\n${err}`, "error");
+                reject();
+            });
+    });
+};
+
+const RobotDeletor = ({robot, onRobotDeleted}) => {
+    const [dialogOpen, setDialogOpen] = useState(false);
+
+    const onConfirmDelete = () => {
+        deleteRobot(robot.id)
+            .then(() => {
+                setDialogOpen(false);
+                onRobotDeleted();
+            });
+    };
+
+    const onClickDelete = () => setDialogOpen(true);
+
+    return (
+        <>
+            <IconButton onClick={onClickDelete}>
+                <DeleteIcon/>
+            </IconButton>
+
+            <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+                <DialogTitle>Delete Robot</DialogTitle>
+
+                <Container>
+                    <List>
+                        <ListItemText>
+                            Are you sure you want to delete this robot?
+                        </ListItemText>
+
+                        <ListItem>
+                            <Typography style={{color: "red"}} variant="caption">
+                                This action cannot be undone.
+                            </Typography>
+                        </ListItem>
+
+                        <ListItemSpreadingChildren>
+                            <Button variant="contained" color="error" onClick={onConfirmDelete}>
+                                Yes
+                            </Button>
+
+                            <Button variant="outlined" onClick={() => setDialogOpen(false)}>
+                                No, go back
+                            </Button>
+                        </ListItemSpreadingChildren>
+                    </List>
+                </Container>
+            </Dialog>
+        </>
+    );
+};
+
+export default RobotDeletor;
---
+++ src/components/robot/RobotTypeDeletor.js
@@ -0,0 +1,107 @@
+import {
+    Button,
+    Container,
+    Dialog,
+    DialogTitle,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Typography
+} from "@mui/material";
+import {useState} from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
+import {ListItemSpreadingChildren} from "../common/StyledComponents";
+import FetchHandler from "../common/FetchHandler";
+import Settings from "../common/settings";
+import Notifications from "../common/Notifications";
+
+
+const deleteRobotType = (id) => {
+    const fetchProps = {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json"}
+    };
+    const urlParams = new URLSearchParams({_id: id});
+    return new Promise((resolve, reject) => {
+        FetchHandler.simple(fetch(`${Settings.robotTypePath}/:id/?${urlParams}`, fetchProps))
+            .then(() => {
+                Notifications.notify("Robot Type deleted", "success");
+                resolve();
+            })
+            .catch((err) => {
+                Notifications.notify(`Failed to delete Robot Type\n${err}`, "error");
+                reject();
+            })
+    });
+};
+
+const RobotTypeDeletor = ({robotType, onRobotTypeDeleted}) => {
+    const [dialogOpen, setDialogOpen] = useState(false);
+
+    const stopBubble = (evt) => {
+        evt.stopPropagation();
+    };
+
+    const onConfirmDelete = () => {
+        deleteRobotType(robotType.id)
+            .then(() => {
+                setDialogOpen(false);
+                onRobotTypeDeleted();
+            });
+    };
+
+    const onClose = (evt) => {
+        stopBubble(evt);
+        setDialogOpen(false);
+    };
+
+    const onClickDelete = (evt) => {
+        stopBubble(evt);
+        setDialogOpen(true);
+    };
+
+    return (
+        <>
+            <IconButton onClick={onClickDelete}>
+                <DeleteIcon/>
+            </IconButton>
+
+            <Dialog open={dialogOpen} onClose={onClose}>
+                <DialogTitle>Delete Robot Type <i>{robotType.name}</i></DialogTitle>
+
+                <Container onClick={stopBubble}>
+                    <List>
+                        <ListItemText>
+                            Are you sure you want to delete this robot type?
+                        </ListItemText>
+
+                        <ListItem>
+                            <Typography style={{color: "red"}} variant="caption">
+                                This action will also remove all robots of the deleted type.
+                            </Typography>
+                        </ListItem>
+
+                        <ListItem>
+                            <Typography style={{color: "red"}} variant="caption">
+                                This action cannot be undone.
+                            </Typography>
+                        </ListItem>
+
+                        <ListItemSpreadingChildren>
+                            <Button variant="contained" color="error" onClick={onConfirmDelete}>
+                                Yes
+                            </Button>
+
+                            <Button variant="outlined" onClick={onClose}>
+                                No, go back
+                            </Button>
+                        </ListItemSpreadingChildren>
+                    </List>
+                </Container>
+            </Dialog>
+        </>
+    );
+};
+
+export default RobotTypeDeletor;
