56e5c0969214b04f169b1bfb63844f4cf07d6b2c
dittmanndennis <dennis1dittmann@gmail.com>
2022-07-11 12:42:40 +0200

    fix npm warning

--- src/components/generator/EditDialog.js
+++ src/components/generator/EditDialog.js
@@ -61,7 +61,7 @@ const EditDialog = () => {
               Notifications.notify(`Failed to retrieve projects\n${err}`, "error");
               setProjectRetrievalState("failed");
           });
-      }, []);
+      }, [id]);
 
       useEffect(() => {
         FetchHandler.readingJson(fetch(Settings.robotPath, {method: "GET"}))
