a35a4d038536e53082ab8acc33c42ca92480176a
dolevi-tub <d.genz@campus.tu-berlin.de>
2022-07-22 11:36:28 +0200

    Cleanup project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import styled from "styled-components";
 import FrontPage from "./components/FrontPage";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import {Grid} from "@mui/material";
 
-const AppRoot = styled.div`
-  display: flex;
-  align-items: center;
-  min-height: 100vh;
-`;
-
-const App = () => {
-
-    return (
-        <AppRoot>
-            <FrontPage/>
-            <ToastContainer limit={3}/>
-        </AppRoot>
-    );
-};
+const App = () => (
+    <Grid container display="flex" alignItems="center" minHeight="100vh">
+        <FrontPage/>
+        <ToastContainer limit={3}/>
+    </Grid>
+);
 
 export default App;
